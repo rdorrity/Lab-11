99f347db743414c8e2e9932626e43512e7f2f94b
Cody Young <cyoung1623@gmail.com>
2018-11-24 21:45:32 -0800

    Added and tweaked Room class to main file. Incorporated Inventory class.

--- Lab11.py
+++ Lab11.py
@@ -67,26 +67,63 @@ class Map:
 ##############################################################
 #############         Cody Young          ####################
  
+class Room:
+	# Class variables:
+	# xpos: X coordinate of the room on the map.
+	# ypos: Y coordinate of the room on the map.
+	# room_items: List of items in room. Inventory class object. 
+	# roomitem_count: Number of items in room.
+	# name: Name of room.
+	# directions: Dictionary of valid directions for player movement in or out of room.
+	# description: Room flavor text.
+	# hint: Hint text. 
+	
+	# Class "constructor"
+	# Initializes room's name, x/y coordinates, and number of items.
+	# Note: A player object, theoretically, will always start in a room at (0,0).
+	def __init__(self, name, xpos, ypos, item_count, description):
+		self.xpos = xpos
+		self.ypos = ypos
+		self.name = ""
+		self.room_items = Inventory()
+		self.item_count = len(room_items)
+		self.directions = {'directions': 'north', 'south', 'east', 'west'}
+		self.description = ""
+		
+	# Returns the x and y coordinates of the room's position.
+	def getPosition(self):
+		return self.xpos
+		return self.ypos
+	
+	# Sets a room's loation using a random coordinate within the bounds of the map.
+	def randomPosition(self, name):
+		# Min/max ranges for x and y axes. Can be changed as necessary (use map class function?)
+		x_min = -10
+		x_max = 10
+		y_min = -10
+		y_max = 10
+	
+		# X coordinate
+		x_rand = random.randint(x_min, x_max + 1)
+		# Y coordinate
+		y_rand = random.randint(y_min, y_max + 1)
+		
+		self.xpos = x_rand
+		self.ypos = y_rand 
+	
+	# Returns true if room has items, else false. 
+	def hasItems(self):
+		if self.room_items > 0:
+			return True
+		else:
+			return False 
+	
+	# Checks room's valid directions for player movement.
+	# Adds or removes room's valid directions (dictionary)
+	# depending if player has reached x or y bound of map (a.k.a. can't move further in one direction)
+	
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	
 ##############################################################
 ##############################################################
 #############     Nathan Warren-Acord     ####################
@@ -139,29 +176,26 @@ class Inventory:
     
 
     
-class Room:
+#class Room:
   
-  def __init__(self, name, room_items, description):
-    self.name = name
-    self.room_items = room_items # just testing inventory. This will be empty under final version.
-    self.description = description
-     
-  def add_item(self, item):
-    self.room_items.append(item)
-     
-  def get_item(self, item):
-    index = self.room_items.index(item)
-    return self.room_items.pop(index)
-     
-  def look(self):
-    return self.room_items
-     
-  def destroy(self, item):  
-    self.room_items.remove(item)
-   
-
-    
-  
+#  def __init__(self, name, room_items, description):
+#    self.name = name
+#    self.room_items = room_items # just testing inventory. This will be empty under final version.
+#    self.description = description
+#     
+#  def add_item(self, item):
+#    self.room_items.append(item)
+#     
+#  def get_item(self, item):
+#    index = self.room_items.index(item)
+#    return self.room_items.pop(index)
+#     
+#  def look(self):
+#    return self.room_items
+#     
+#  def destroy(self, item):  
+#    self.room_items.remove(item)
+#   
 
 def ryanPlay():
   
