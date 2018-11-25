e1abfb8d2aa56337c628ca006fd28ed277bf1302
rdorrity <37189242+rdorrity@users.noreply.github.com>
2018-11-25 11:45:41 -0800

    Update Lab11.py
    I went through our master file and indented everything so it would compile correctly and we can added newlines without issues. I also had add parameters and comment out a few things to make everything work.

--- Lab11.py
+++ Lab11.py
@@ -68,59 +68,61 @@ class Map:
 #############         Cody Young          ####################
  
 class Room:
-	# Class variables:
-	# xpos: X coordinate of the room on the map.
-	# ypos: Y coordinate of the room on the map.
-	# room_items: List of items in room. Inventory class object. 
-	# roomitem_count: Number of items in room.
-	# name: Name of room.
-	# directions: Dictionary of valid directions for player movement in or out of room.
-	# description: Room flavor text.
-	# hint: Hint text. 
+# Class variables:
+# xpos: X coordinate of the room on the map.
+# ypos: Y coordinate of the room on the map.
+# room_items: List of items in room. Inventory class object. 
+# roomitem_count: Number of items in room.
+# name: Name of room.
+# directions: Dictionary of valid directions for player movement in or out of room.
+# description: Room flavor text.
+# hint: Hint text. 
+
+# Class "constructor"
+# Initializes room's name, x/y coordinates, and number of items.
+# Note: A player object, theoretically, will always start in a room at (0,0).
+#def __init__(self, name, xpos, ypos, item_count, description)
+  def __init__(self, name, xpos, ypos, description):
+    self.xpos = xpos
+    self.ypos = ypos
+    self.name = ""
+    self.room_items = Inventory()
+    #self.item_count = len(room_items)
+    #self.directions = {'directions': 'north', 'south', 'east', 'west'}
+    self.description = ""
+    self.directions = ['north', 'south', 'east', 'west']
+  
+    # Returns the x and y coordinates of the room's position.
+    def getPosition(self):
+      return self.xpos
+      return self.ypos
+
+    # Sets a room's loation using a random coordinate within the bounds of the map.
+    def randomPosition(self, name):
+    # Min/max ranges for x and y axes. Can be changed as necessary (use map class function?)
+      x_min = -10
+      x_max = 10
+      y_min = -10
+      y_max = 10
 	
-	# Class "constructor"
-	# Initializes room's name, x/y coordinates, and number of items.
-	# Note: A player object, theoretically, will always start in a room at (0,0).
-	def __init__(self, name, xpos, ypos, item_count, description):
-		self.xpos = xpos
-		self.ypos = ypos
-		self.name = ""
-		self.room_items = Inventory()
-		self.item_count = len(room_items)
-		self.directions = {'directions': 'north', 'south', 'east', 'west'}
-		self.description = ""
-		
-	# Returns the x and y coordinates of the room's position.
-	def getPosition(self):
-		return self.xpos
-		return self.ypos
+    # X coordinate
+      x_rand = random.randint(x_min, x_max + 1)
+    # Y coordinate
+      y_rand = random.randint(y_min, y_max + 1)
+
+      self.xpos = x_rand
+      self.ypos = y_rand 
+
+    # Returns true if room has items, else false. 
+    def hasItems(self):
+      if self.room_items > 0:
+        return True
+      else:
+        return False 
 	
-	# Sets a room's loation using a random coordinate within the bounds of the map.
-	def randomPosition(self, name):
-		# Min/max ranges for x and y axes. Can be changed as necessary (use map class function?)
-		x_min = -10
-		x_max = 10
-		y_min = -10
-		y_max = 10
-	
-		# X coordinate
-		x_rand = random.randint(x_min, x_max + 1)
-		# Y coordinate
-		y_rand = random.randint(y_min, y_max + 1)
-		
-		self.xpos = x_rand
-		self.ypos = y_rand 
-	
-	# Returns true if room has items, else false. 
-	def hasItems(self):
-		if self.room_items > 0:
-			return True
-		else:
-			return False 
-	
-	# Checks room's valid directions for player movement.
-	# Adds or removes room's valid directions (dictionary)
-	# depending if player has reached x or y bound of map (a.k.a. can't move further in one direction)
+# Checks room's valid directions for player movement.
+# Adds or removes room's valid directions (dictionary)
+# depending if player has reached x or y bound of map (a.k.a. can't move further in one direction)
 	
 
 	
@@ -209,12 +211,12 @@ def main():
            [3,4,5],\
            [6,7,8]])
   print m.__getitem__(2,2)
-  main_room = Room("Main Room", [],
+  main_room = Room("Main Room", 1, 1,
                      "You wake up in a weird room. There are doors to the North, South, East, and West.")
-  north_room = Room("North Room", [], "You are in the North Room")
-  south_room = Room("South Room", [], "You are in the South Room")
-  east_room = Room("East Room", [], "You are in the East Room")
-  west_room = Room("West Room", [], "You are in the West Room")
+  north_room = Room("North Room", 0, 1, "You are in the North Room")
+  south_room = Room("South Room", 2, 1, "You are in the South Room")
+  east_room = Room("East Room", 2, 2, "You are in the East Room")
+  west_room = Room("West Room", 1, 0, "You are in the West Room")
   m.add(north_room, 0, 1)
   m.add(west_room, 1, 0)
   m.add(main_room, 1, 1)
